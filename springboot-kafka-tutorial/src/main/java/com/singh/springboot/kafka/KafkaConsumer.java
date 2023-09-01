55286847469ebee8cde89f5db045326c91f9b353
Siddharth Singh <54412467+Siddharth0701@users.noreply.github.com>
2023-09-01 11:41:43 +0530

    Created Apache kafka project

---
+++ springboot-kafka-tutorial/src/main/java/com/singh/springboot/kafka/KafkaConsumer.java
@@ -0,0 +1,18 @@
+package com.singh.springboot.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class KafkaConsumer {
+    private static  final Logger LOGGER= LoggerFactory.getLogger(KafkaConsumer.class);
+    @KafkaListener(topics = "singh",groupId = "myGroup")
+    public void consume(String message){
+        LOGGER.info(String.format("Message received -> %s",message));
+
+    }
+}
